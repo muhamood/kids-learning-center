4d62d825a39dcbe19e92b79e961943f00443110a
muhamood <nmuhamood3@gmail.com>
2021-03-18 13:33:27 +0300

    setup navbar layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+     <Navbar/>
      <h1>KIDS CENTER</h1>
     </div>
   );
